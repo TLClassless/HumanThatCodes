391d13d2a941b5d754d4b7d5b6016b3735005d81
Ben <34322681+TLClassless@users.noreply.github.com>
2020-08-01 14:48:49 +0100

    initial commit

---
+++ js/helloFunc.js
@@ -0,0 +1,11 @@
+let words = ["Hello", "Hallo", "Olá", "Привет", "你好", "Ahoj", "Bonjour", "Kamusta", "Hei", "Aloha", "Ciao", "こんにちは", "여보세요", "Hallå"];
+
+let getRandomWord = function () {
+    return words[Math.floor(Math.random() * words.length)];
+};
+
+let word = getRandomWord();
+
+console.log(word);
+
+document.getElementById("hello").textContent = word;
